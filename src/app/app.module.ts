69c04c452eabca610a329644bfec1d20f3061461
Ho3einzargar <Ho3einzargar@yahoo.com>
2021-11-27 12:00:30 +0330

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
+import { FormsModule } from '@angular/forms';
+import { PowerBoostCalculatorComponent } from './power-boost-calculator/power-boost-calculator.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroBirthday2Component } from './hero-birthday2/hero-birthday2.component';
+import { ExponentialStrengthPipe } from './exponential-strength.pipe';
+import { PowerBoosterComponent } from './power-booster/power-booster.component';
+import { FlyingHeroesComponent } from './flying-heroes/flying-heroes.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroBirthday2Component,
+    ExponentialStrengthPipe,
+    PowerBoosterComponent,
+    PowerBoostCalculatorComponent,
+    FlyingHeroesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
